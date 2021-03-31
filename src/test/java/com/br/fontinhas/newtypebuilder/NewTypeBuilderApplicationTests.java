fb3f3969bad93ec26d9c80ed2641cc39bf60028f
leonardo.fontinhas <leonardo.fontinhas@db1.com.br>
2021-03-31 17:30:32 -0300

    init project

---
+++ src/test/java/com/br/fontinhas/newtypebuilder/NewTypeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fontinhas.newtypebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTypeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
